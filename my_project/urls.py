00bc96425e6bc64be6e1ea86c0a3422b96563a62
Alan <alanjameschapman@gmail.com>
2024-01-28 10:39:12 +0000

    Add about page view and URL mapping

--- my_project/urls.py
+++ my_project/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include # new
 from hello_world import views as index_views # new
+from about import views as about_views # new
+
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
+    path('hello/', index_views.index, name='index'),
+    path('about/', about_views.about_me, name='about'),
     path('admin/', admin.site.urls),
 ]
